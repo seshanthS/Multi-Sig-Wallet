80fd8751cb9807ed8b9f1c986552a94434988a61
Seshanth.S <35675963+seshanthS@users.noreply.github.com>
2018-10-17 20:00:19 +0530

    Update wallet.sol

--- wallet.sol
+++ wallet.sol
@@ -1,13 +1,23 @@
 pragma solidity ^0.4.24;
 
 contract wallet {
+    
+    uint8 minNeeded;
     address owner;
     mapping(address => uint)private allowedOwners;
     //transactionId, addresses_who_signed, signature_Status(1 or 0)
     mapping(uint => mapping (address => uint )) signatures;
+    //txId, to address
+    mapping(uint => address) idToaddress;
+    //txId, no.of.Approvals;
+    mapping(uint => uint8) private approval;
     
-    constructor() public {
+    event approvalNeeded(uint _txId, address _from, address _to, uint _amount, string _data);
+    
+    constructor() public payable {
         owner = msg.sender;
+        allowedOwners[owner] = 1;
+        minNeeded = 1;
     }
     
     modifier onlyAllowedOwners {
@@ -29,5 +39,29 @@ contract wallet {
     
     function() public payable {
         
+    }
+    
+    function transfer(address _to, uint _amount, string _data) public onlyAllowedOwners {
+        uint txId;
+        //for testing, lets increment txid automatically by 1;
+        txId ++;
+        //TODO : assign txHash
+        idToaddress[txId] = _to;
+        requestApproval(txId, msg.sender, _to, _amount, _data);
+    }
+    
+    function _transfer(address _to) internal {
+        address(_to).transfer(msg.value);
+    }
+    
+    function requestApproval(uint _txId, address _from, address _to, uint _amount, string _data) private {
+        emit approvalNeeded(_txId, _from, _to, _amount, _data);
+    }
+    
+    function approve(uint _txId) public onlyAllowedOwners {
+        approval[_txId] +=1 ;
+        if ( approval[_txId] >= minNeeded ) {
+            _transfer(idToaddress[_txId]);
+        }
     }
 }
