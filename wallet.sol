5874dc6be54528335fdd596706cfa82d42a32837
Seshanth.S <35675963+seshanthS@users.noreply.github.com>
2018-10-17 15:00:49 +0530

    Update wallet.sol

--- wallet.sol
+++ wallet.sol
@@ -1,1 +1,33 @@
+pragma solidity ^0.4.24;
 
+contract wallet {
+    address owner;
+    mapping(address => uint)private allowedOwners;
+    //transactionId, addresses_who_signed, signature_Status(1 or 0)
+    mapping(uint => mapping (address => uint )) signatures;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyAllowedOwners {
+        require(allowedOwners[msg.sender] == 1);
+        _;
+    }
+    
+    function addOwner(address _newOwner) public onlyAllowedOwners {
+        allowedOwners[_newOwner] = 1;
+    }
+    
+    function revokeOwner(address _oldOwner) public onlyAllowedOwners {
+        allowedOwners[_oldOwner] = 0;
+    }
+    
+    function balance() public view onlyAllowedOwners returns (uint) {
+        return address(this).balance;
+    }
+    
+    function() public payable {
+        
+    }
+}
