acf488abae37334a840a30fe514420af845b40f8
Seshanth.S <35675963+seshanthS@users.noreply.github.com>
2018-10-18 14:03:55 +0530

    Update wallet.sol

--- wallet.sol
+++ wallet.sol
@@ -13,16 +13,25 @@ contract wallet {
     mapping(uint => uint8) private approval;
     //txid, amount;
     mapping (uint => uint) txAmount;
-    
+    // NEW BRANCH >>
     struct transaction {
+        uint8 signatureCount;
         uint id;
         uint amount;
         address _to;
         address _from;
         string status;
         string data;
+        address[] signatures;
+       
     }
+    //txId, transaction
+    mapping(uint  => transaction) tx;
+    uint[] private transactionList;
     
+     // NEW BRANCH -ENDS <<
+     
+     
     event approvalNeeded(uint _txId, address _from, address _to, uint _amount, string _data);
     event transactionComplete(uint _txId);
     event gotRequiredSignatures(uint _txId);
@@ -56,6 +65,20 @@ contract wallet {
     
     function transfer(address _to, string _data) public onlyAllowedOwners payable {
         uint txId;
+        txId++;
+        transaction memory t1;
+        t1.id = txId;
+        t1.amount = msg.value;
+        t1._to = _to;
+        t1._from = msg.sender;
+        t1.status = "Initiated";
+        t1.data = _data;
+        
+        tx[txId] = t1;
+        
+        requestApproval(t1.id);
+        /*
+        uint txId;
         //for testing, lets increment txid automatically by 1;
         txId ++;
         //TODO : assign txHash
@@ -64,6 +87,7 @@ contract wallet {
         amount = msg.value;
         txAmount[txId] = amount;
         requestApproval(txId, msg.sender, _to, _data);
+        */
     }
     
     function _transfer(address _to,uint _amount, uint _txId) internal {
@@ -71,17 +95,24 @@ contract wallet {
         emit transactionComplete(_txId);
     }
     
-    function requestApproval(uint _txId, address _from, address _to, string _data) private {
-       uint _amount = txAmount[_txId];
-        emit approvalNeeded(_txId, _from, _to, _amount, _data);
+    function requestApproval(uint _txId) private {
+       transaction memory t1 = tx[_txId];
+       emit approvalNeeded (t1.id, t1._from, t1._to, t1.amount, t1.data);
+        //emit approvalNeeded(_txId, _from, _to, _amount, _data);
     }
     
     function approve(uint _txId) public onlyAllowedOwners {
+        transaction memory t1 = tx[_txId];
+        t1.signatureCount++;
+        address[] memory t = new t1.signatures[];
+        t1.signatures[].push(msg.sender);
+        /*
         approval[_txId] +=1 ;
         uint amount = txAmount[_txId];
         if ( approval[_txId] >= minNeeded ) {
             emit gotRequiredSignatures(_txId);
             _transfer(idToaddress[_txId], amount, _txId);
         }
+        */
     }
 }
